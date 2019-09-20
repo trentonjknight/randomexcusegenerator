35b34178ef9a1247470232e07ad76926bf637787
Trenton Knight <tknightj1995@yahoo.com>
2019-09-20 22:37:55 +0000

    randomgeneratorcreated

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,40 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+
+  let generateExcuse = () => {
+    let culprit = [
+      "My Dad",
+      "My brother Kyle",
+      "My Mom",
+      "My dog Lucy",
+      "My cat Chicha"
+    ];
+
+    let what = [" destroyed ", " ate ", " stole "];
+    let thing = [" my shoes ", " my clothes "];
+    let when = [" today ", " a few hours ago ", " yesterday "];
+    let where = [" at home ", " on the street ", " at the movies "];
+
+    let culpritIndex = Math.floor(Math.random() * culprit.length);
+    let whatIndex = Math.floor(Math.random() * what.length);
+    let thingIndex = Math.floor(Math.random() * thing.length);
+    let whenIndex = Math.floor(Math.random() * when.length);
+    let whereIndex = Math.floor(Math.random() * where.length);
+
+    return (
+      culprit[culpritIndex] +
+      "" +
+      what[whatIndex] +
+      "" +
+      thing[thingIndex] +
+      "" +
+      when[whenIndex] +
+      "" +
+      where[whereIndex]
+    );
+  };
 };
